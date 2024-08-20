7b5999dad48e5f7f29281f43f22c054fa657d4f5
Ryan Thurston <55929023+thurstonr18@users.noreply.github.com>
2024-08-19 22:13:13 -0400

    Up to creating venv, including installation of dramatiq & simple_history

--- Deploy.py
+++ Deploy.py
@@ -1,19 +1,30 @@
+
 import os, sys, re
 import subprocess
+from random import random
 from subprocess import Popen as pop
+import time
 
 bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
 rrico = os.path.join(bundle_dir, 'rr.ico')
 
+
+class SubprocessErrorDetected(Exception):
+    def __init__(self, message=None):
+        self.message = message
+        super().__init__(self.message)
+
+
 class Deployer:
     def __init__(self, answers):
-        self.password = answers['password']
-        self.features = answers['features']
-        self.project_name = answers['name']
-        self.path = f"{answers['path']}:/"
-        self.psql_path = answers['psql']
-        self.project_path = os.path.join(self.path, self.project_name) #Top directory (e.g. Z:/Avit-internal-apps); does not include Django root
+
+        self.features = answers['project_features']
+        self.project_name = answers['project_name']
+        self.project_path = answers['project_drive']
+        self.psql_path = answers['sql_path']
+        self.password = answers['sql_pw']
         self.django_path = None
+        self.venv_path = os.path.join(self.project_path, 'venv')
 
 
     def process(self, cmd, shell, wdir=None):
@@ -34,26 +45,87 @@ class Deployer:
             query = error
 
         return query
-
-    def setup_db(self):
-        cmd = [
-            self.psql_path,
-            '-U postgres',
-            '-c', f"CREATE DATABASE [IF NOT EXISTS] {self.project_name}"
-        ]
+    def create_venv(self):
+        # Command to create a virtual environment
+        cmd = f"py -3.11 -m venv {self.venv_path}"
+        #print(f"Creating virtual environment at {self.venv_path}...")
         self.process(cmd, 'Powershell')
 
-        return
+    def activate_venv(self):
+        # Activation script depends on the OS
+        if sys.platform == "win32":
+            activate_script = os.path.join(self.venv_path, 'Scripts', 'Activate.ps1')
+        else:
+            activate_script = os.path.join(self.venv_path, 'bin', 'activate')
+
+        # Ensure the activation command is used for the subsequent commands
+        return activate_script
+
+    def setup_db(self):
+        env = os.environ.copy()
+        env['PGPASSWORD'] = self.password
+        cmd = [
+            self.psql_path,
+            '-U' 'postgres',
+            '-c', f"CREATE DATABASE {self.project_name};"
+        ]
+        p = pop(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+        out, err = p.communicate()
+        output = out.decode('utf8')
+        if p.returncode != 0:
+            raise SubprocessErrorDetected(message=err.decode('utf8'))
+
+        return True
+
+
+    def pee_pee(self):
+        for i in range(100):
+            yield "pee pee {}".format(str(i))
+            time.sleep(0.05)
 
     def deploy(self):
+        # Set up the database
+        yield "Creating DB..."
         self.setup_db()
+        time.sleep(1)
 
-        os.mkdir(self.project_path)
-        cmd = f"django-admin startproject {self.project_name}"
+
+        # Create the virtual environment
+        yield "Creating venv"
+        self.create_venv()
+        time.sleep(1)
+
+        # Activate the virtual environment
+        activate_script = self.activate_venv()
+        activate_cmd = f"& {activate_script}"
+
+        # Create the project directory
+        yield "Make dir"
+        os.mkdir(os.path.join(self.project_path, self.project_name))
+        time.sleep(1)
+
+        # Run the Django commands within the virtual environment
+        yield "Run startproject"
+        cmd = f"{activate_cmd}; django-admin startproject {self.project_name}"
         self.process(cmd, 'Powershell', wdir=self.project_path)
+        time.sleep(1)
+
         self.django_path = os.path.join(self.project_path, self.project_name)
-        cmd = "django-admin startapp main"
+
+        yield "Run startapp"
+        cmd = f"{activate_cmd}; django-admin startapp main"
         self.process(cmd, 'Powershell', wdir=self.django_path)
+
+        if "Dramatiq" in self.features:
+            yield "Install Dramatiq"
+            cmd = f"{activate_cmd}; python -m pip install django-dramatiq[rabbitmq]"
+            self.process(cmd, 'Powershell', wdir=self.django_path)
+            time.sleep(1)
+        if "Simple History" in self.features:
+            yield "Install Simple History"
+            cmd = f"{activate_cmd}; python -m pip install django-simple-history"
+            self.process(cmd, 'Powershell', wdir=self.django_path)
+            time.sleep(1)
 
 
 
--- Inquire.py
+++ Inquire.py
@@ -1,16 +1,33 @@
-import os, sys, re, time
+import os
+import sys
+import re
+import time
+import json
 import subprocess
-import winreg
 from subprocess import Popen as pop
 from PyInquirer import prompt
 from Deploy import Deployer
-
+from tqdm import tqdm
 
 class PostgreSQLNotInstalledError(Exception):
     def __init__(self, message="PostgreSQL is not installed. Please install PostgreSQL to proceed."):
         self.message = message
         super().__init__(self.message)
 
+class ErlangNotInstalledError(Exception):
+    def __init__(self, message="Erlang is not installed. Please install Erlang to proceed."):
+        self.message = message
+        super().__init__(self.message)
+
+class RabbitMQNotInstalledError(Exception):
+    def __init__(self, message="RabbitMQ is not installed. Please install RabbitMQ to proceed."):
+        self.message = message
+        super().__init__(self.message)
+
+class PostgreSQLDatabaseExistsError(Exception):
+    def __init__(self, message="CREATE DATABASE FAILURE. Database already exists."):
+        self.message = message
+        super().__init__(self.message)
 
 class Questionnaire:
     def __init__(self):
@@ -19,6 +36,7 @@ class Questionnaire:
         out, err = dirs.communicate()
         output = out.decode('utf8')
         self.sql_path = None
+        self.rabbitmq_path = None
         self.drives = output.splitlines()
         self.questions = [
             {
@@ -62,72 +80,138 @@ class Questionnaire:
             Follow the prompts to set up your Django project and streamline your deployment process.
             Let’s get started!
             """
-
+        self.responses = {}
         print(intro_message)
         time.sleep(2)
 
-    def find_psql(self):
+    def is_database(self, name=None, pwd=None, sqlpath=None):
+        env = os.environ.copy()
+        env['PGPASSWORD'] = pwd
+        cmd = [
+            sqlpath,
+            '-U' 'postgres',
+            '-c', f"SELECT datname FROM pg_database;"
+        ]
+        p = pop(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+        out, err = p.communicate()
+        output = out.decode('utf8')
+        r0 = re.search(rf'{name}', output, re.IGNORECASE)
+        if r0 is not None:
+            return False
+        print(f"\nDatabase {name} is ready")
+        time.sleep(1)
+        return True
+
+    def is_postgres(self):
         common_paths = [
             r'C:\Program Files\PostgreSQL',
             r'C:\Program Files (x86)\PostgreSQL'
         ]
-
         for path in common_paths:
             if os.path.exists(path):
                 try:
-                    # List all directories in the base path
                     for version_dir in sorted(os.listdir(path)):
-                        version_path = os.path.join(path, version_dir, 'bin', 'psql.exe')
+                        version_path = os.path.join(path, str(version_dir), 'bin', 'psql.exe')
                         if os.path.isfile(version_path):
-                            return version_path
+                            self.sql_path = str(version_path)
+                            print(f"\nPostgreSQL is installed. Found psql at: {self.sql_path}")
+                            time.sleep(1)
+                            return True
                 except PermissionError:
+                    print('\nNo access allowed')
+                    continue
+        print("\nPostgreSQL is not installed.")
+        return False
+
+    def find_rabbitmqctl(self):
+        common_paths = [
+            r'C:\Program Files\RabbitMQ',
+            r'C:\Program Files\RabbitMQ Server',
+            r'C:\Program Files (x86)\RabbitMQ',
+            r'C:\Program Files (x86)\RabbitMQ Server'
+        ]
+        for path in common_paths:
+            if os.path.exists(path):
+                try:
+                    for version_dir in sorted(os.listdir(path)):
+                        bin_path = os.path.join(path, str(version_dir), 'sbin', 'rabbitmqctl.bat')
+                        if os.path.isfile(bin_path):
+                            self.rabbitmq_path = str(bin_path)
+                            return
+                except PermissionError:
+                    print('\nNo access allowed')
                     return None
         return None
 
-    def is_postgres(self):
-        #print('\nPlease wait..')
-        time.sleep(2)
-        self.find_psql()
-        if self.sql_path:
-            #print(f"\nPostgreSQL is installed. Found psql at: {psql_path}")
+    def check_erlang(self):
+        try:
+            result = subprocess.run(["erl", "-eval", "erlang:display(erlang:system_info(otp_release)), halt()"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            otp_release = result.stdout.decode().strip()
+            print(f"\nErlang is installed. Found Erlang OTP release: {otp_release}")
+            time.sleep(.5)
             return True
-        else:
-            #print("\nPostgreSQL is not installed.")
+        except subprocess.CalledProcessError:
             return False
+
+    def check_rabbitmq(self):
+        self.find_rabbitmqctl()
+        if self.rabbitmq_path:
+            try:
+                subprocess.run([self.rabbitmq_path, "status"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print(f"\nRabbitMQ is installed. Found rabbitmqctl at: {self.rabbitmq_path}")
+                time.sleep(1)
+                return True
+            except subprocess.CalledProcessError:
+                return False
+        return False
 
     def ask_it(self):
         while True:
-            answers = prompt(self.questions)
-            fpath = f"{answers['path']}:/"
-            ppath = os.path.join(fpath, answers['name'])
-            if os.path.isdir(ppath):
-                print(f"Error: The directory '{ppath}' already exists. Please choose a different project name or drive.")
-                continue  # Restart the questions if the directory exists
-            if not self.is_postgres():
-                raise PostgreSQLNotInstalledError(f"Error: PostgreSQL is not installed.\n\nIt appears that the required PostgreSQL database server is missing from your system. Please install PostgreSQL to proceed.")
-            answers.update(
-                {
-                    'psql': str(self.sql_path)
-                }
-            )
-            return answers
-
-
-
+            try:
+                answers = prompt(self.questions)
+                fpath = f"{answers['path']}:/"
+                ppath = os.path.join(fpath, answers['name'])
+                if os.path.isdir(ppath):
+                    print(f"\nError: The directory '{ppath}' already exists. Please choose a different project name or drive.")
+                    continue
+                if not self.is_postgres():
+                    raise PostgreSQLNotInstalledError()
+                if not self.is_database(name=answers['name'], pwd=answers['password'], sqlpath=str(self.sql_path)):
+                    raise PostgreSQLDatabaseExistsError()
+                self.responses.update(
+                    {
+                        'project_name': answers['name'],
+                        'project_drive': ppath,
+                        'project_features': answers['features'],
+                        'sql_pw': answers['password'],
+                        'sql_path': str(self.sql_path),
+                        'confirm': answers['confirm']
+                    }
+                )
+                if "Dramatiq" in answers['features']:
+                    if not self.check_erlang():
+                        raise ErlangNotInstalledError()
+                    if not self.check_rabbitmq():
+                        raise RabbitMQNotInstalledError()
+                return self.responses
+            except Exception as e:
+                print(e)
+                return 'HELP'
 
 if __name__ == "__main__":
     ppath = None
-
     q = Questionnaire()
     while True:
         try:
             qa = q.ask_it()
-
+        except Exception as e:
+            print(e)
+            exit()
+        try:
+            confirm = qa['confirm']
         except:
             exit()
-
-        # Check if the user confirms the deployment
-        if qa['confirm']:
+        if confirm:
             break
         else:
             restart_prompt = {
@@ -138,11 +222,13 @@ if __name__ == "__main__":
             }
             restart = prompt(restart_prompt)
             if not restart['restart']:
-                exit()  # Exit the program if the user doesn't want to restart
-
+                exit()
     try:
-        #dep = Deployer(qa)
-        #dep.deploy()
+        dep = Deployer(qa)
+        progress_bar = tqdm(dep.deploy(), desc='Deploying...', total=100)
+        for proc in progress_bar:
+            progress_bar.set_description(proc)
+            progress_bar.update()
         print('Done')
     except Exception as e:
         print(f"An error occurred while deploying the project, please see below.\n\n{str(e)}")
